1df6bb2779005300ffd48327dbfe11a1b9d72f28
sreedharbabuk <babjibabu.k@gmail.com>
2017-01-04 16:12:14 +0530

    initial check in java program

---
+++ Hello-World-JavaApp/src/helloworld/HelloWorldJava.java
@@ -0,0 +1,11 @@
+package helloworld;
+
+public class HelloWorldJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello git and java world");
+	}
+
+}
